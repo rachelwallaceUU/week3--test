6294983854aa66d93b76f162b0aa472b246a00ae
jpconnolly <jp.connolly@ulster.ac.uk>
2024-10-14 21:48:58 +0100

    Add files via upload

---
+++ Library/Book.cs
@@ -0,0 +1,45 @@
+﻿namespace Library
+{
+    class Book
+    { 
+        private string title; // private variable
+        private string author;// private variable
+        private string isbn; // private variable
+
+        // Title property allows access
+        // to the title private variable
+        public string Title
+        {
+            get { return title; }  // get method
+            set { title = value; } // set method
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+
+        // Example of a constructor that allows us to 'construct' a new
+        // Book object
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            this.Title = bookTitle;
+            this.Author = bookAuthor;
+            this.ISBN = bookISBN;
+        }
+
+        // Method to display information about a book
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Library/Member.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Library
+{
+    public class Member
+    {
+        // private variables
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+
+        // Public properties
+        public int MemberId
+        {
+            get { return memberId; }
+            private set { memberId = value; } // Private setter makes it read-only
+        }
+        public string Name 
+        {
+            get { return name; }  // get method
+            set { name = value; } // set method
+        }
+        public string Address
+        {
+            get { return address; }  // get method
+            set { address = value; } // set method
+        }
+        public int Phone
+        {
+            get { return phone; }  // get method
+            set { phone = value; } // set method
+        }
+
+        // Constructor for new member
+        public Member(int memberId, string name, string address, int phone)
+        {
+            this.MemberId = memberId;
+            this.Name = name;
+            this.Address = address;
+            this.Phone = phone;
+        }
+
+        // Method to display information about a Mamber
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Member ID: {MemberId}");
+            Console.WriteLine($"Member name: {Name}");
+            Console.WriteLine($"Member address: {Address}");
+            Console.WriteLine($"Member phone no: {Phone}");
+            Console.WriteLine();
+        }
+    }
+}
